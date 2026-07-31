dcb09894f19472f0336e958129698390f26ce4a3
CH Shakish <ch.shakish11123@gmail.com>
2026-07-31 11:50:19 +0530

    Prisma and Docker setup

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,9 +1,17 @@
-import { PrismaClient } from "@prisma/client"
+import { PrismaClient } from "../../src/generated/prisma/client"
+import { PrismaPg } from "@prisma/adapter-pg"
+import { Pool } from "pg"
+
+// Set up the connection pool and driver adapter
+const connectionString = `${process.env.DATABASE_URL}`
+const pool = new Pool({ connectionString })
+const adapter = new PrismaPg(pool)
 
 const globalForPrisma = globalThis as unknown as {
   prisma: PrismaClient | undefined
 }
 
-export const prisma = globalForPrisma.prisma ?? new PrismaClient()
+// Pass the adapter to PrismaClient
+export const prisma = globalForPrisma.prisma ?? new PrismaClient({ adapter })
 
 if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma
